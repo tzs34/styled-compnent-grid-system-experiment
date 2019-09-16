d036ee9027e67240b393f85281a6b4157061d758
Dave Thorneycroft <dthorneycroft@goodprcatice.com>
2019-09-16 19:05:52 +0100

    Tidied up code

--- src/App.js
+++ src/App.js
@@ -4,19 +4,18 @@ import {Row, Column} from './components/styled-layout'
 function App() {
   return (
     <div>
-    <Row columns={6}>
-      <Column desktop={1} tablet={2} mobile={6}>
-        <div style={{background:'#AA88EE'}}>One</div>
-      </Column>
-      <Column desktop={2} tablet={2} mobile={6}>
-        <div style={{background:'#AA77AA'}}>Two</div>
-      </Column>
-      <Column desktop={3} tablet={2} mobile={6}>
-        <div style={{background:'#CCEEDD'}}>Three</div>
-      </Column>
-    </Row>
-
-    </div>
+      <Row columns={6}>
+        <Column desktop={1} tablet={2} mobile={6}>
+          <div style={{background:'#AA88EE'}}>One</div>
+        </Column>
+        <Column desktop={2} tablet={2} mobile={6}>
+          <div style={{background:'#AA77AA'}}>Two</div>
+        </Column>
+        <Column desktop={3} tablet={2} mobile={6}>
+          <div style={{background:'#CCEEDD'}}>Three</div>
+        </Column>
+      </Row>
+   </div>
   );
 }
 
