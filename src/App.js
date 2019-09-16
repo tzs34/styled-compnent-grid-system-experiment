7a08324248bf3a9cdbb78430a35b128ede1d3f6c
Dave Thorneycroft <dthorneycroft@goodprcatice.com>
2019-09-16 19:04:48 +0100

    Refactored app to use helpers file

--- src/App.js
+++ src/App.js
@@ -1,33 +1,5 @@
 import React from 'react';
-import styled from 'styled-components'
-import { device } from './helpers/app-helpers';
-
-const Column= styled.div`
-  --width: ${props => props.mobile};
-
-  @media only screen and ${device.tablet}  {
-    --width: ${({mobile, tablet}) => tablet || mobile};
-  }
-
-  @media only screen and  ${device.desktop}{
-    --width: ${({mobile, desktop}) => desktop || mobile};
-  }
-
-`
-
-const Row = styled.div`
-  	margin: 0;
-		display: flex;
-		flex-flow: row wrap;
-		justify-content: space-between;
-  --columns: ${props => props.columns || 4};
-
-  ${Column}{
-    --initialbasis: calc(var(--width, 0) / var(--columns) * 100%);
-    --gap: calc((var(--columns) - var(--width)) * 0.2%);
-    flex-basis: calc(var(--initialbasis) - var(--gap));
-  }
-`
+import {Row, Column} from './components/styled-layout'
 
 function App() {
   return (
--- src/components/styled-layout.js
+++ src/components/styled-layout.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import styled from 'styled-components'
+import { device } from '../helpers/app-helpers';
 
 export const Column= styled.div`
---width: ${props => props.width};
-background: ${props => props.background};
+    --width: ${props => props.width};
+
+
+    @media only screen and ${device.tablet}  {
+        --width: ${({mobile, tablet}) => tablet || mobile};
+    }
+
+    @media only screen and  ${device.desktop}{
+    --width: ${({mobile, desktop}) => desktop || mobile};
+  
 `
 
 export const Row = styled.div`
-    margin: 0;
-      display: flex;
-      flex-flow: row wrap;
-      justify-content: space-between;
---columns: ${props => props.columns || 4};
+  	margin: 0;
+		display: flex;
+		flex-flow: row wrap;
+		justify-content: space-between;
+  --columns: ${props => props.columns || 4};
 
-${Column}{
-  
-  --initialbasis: calc(var(--width, 0) / var(--columns) * 100%);
-  --gap: 1%;/*calc((var(--columns) - var(--width)) * 0.25%);*/
-  flex-basis: calc(var(--initialbasis) - var(--gap));
-}
+  ${Column}{
+    --initialbasis: calc(var(--width, 0) / var(--columns) * 100%);
+    --gap: calc((var(--columns) - var(--width)) * 0.2%);
+    flex-basis: calc(var(--initialbasis) - var(--gap));
+  }
 `
